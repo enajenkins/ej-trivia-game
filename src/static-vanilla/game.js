49700b5f18c708ee866453fdd9f1c76d94c79721
Ena Jenkins <enajenkins@gmail.com>
2022-08-09 22:30:52 -0500

    Added event listener for answer selection click | stored current correct answer | check for max questions and empty question array

--- src/static-vanilla/game.js
+++ src/static-vanilla/game.js
@@ -82,7 +82,8 @@ let questions = [
 
   let availableQuestions = [];
   let currentQuestion = {};
-  let isAcceptingAnswers = true;
+  let isAcceptingAnswers = false; // prevents user from answering before question is loaded
+  let correctAnswer = "";
   let questionCount = 0;
   let score = 0;
 
@@ -105,7 +106,7 @@ let questions = [
     // grab the [questions] array and spread out each of it's items, then enter them into a new array.
     // this will return a full copy of the questions array and will avoid effecting the original array.
     // you can also spread and combine additional arrays into one large one later if desired.
-    availableQuestions = [ ...questions]; 
+    availableQuestions = [ ...questions ]; 
 
     // load new question
     getNewQuestion();
@@ -115,6 +116,14 @@ let questions = [
 /* --- QUESTIONS AND ANSWERS --------- */
 
   const getNewQuestion = () => {
+
+    // check if there are anymore questions left in the [availableQuestions] array which havent been asked 
+    // OR if we have reached the maximum amount of questions allowed. 
+    // if so, navigate to the given url or location
+    if ( availableQuestions.length === 0 || questionCount > MAX_QUESTIONS ) {
+      return window.location.assign('/end.html');
+    }
+
     // update question count when new question is loaded
     questionCount++;
 
@@ -122,24 +131,54 @@ let questions = [
     // return random index for the question set 
     // NOTES: (Math.random() returns a demical number between 0 and 1 | multiplying that result by a number returns another number that can be converted to an integer via ceil() or floor() and used as the index
     // ENHANCEMENT: rather than random questions, refactor for random question sets so questions are not repeated in the game || store the rendered question indexes for the current session (or gamer account) in a collection to check against
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length); 
+    const questionIndex = Math.floor( Math.random() * availableQuestions.length ); 
 
     // get a reference to the current [question] array item using the random index
-    currentQuestion = availableQuestions[questionIndex]; 
+    currentQuestion = availableQuestions[ questionIndex ]; 
 
     // populate the #question element in the ui with the [question] text at the random index
     question.innerText = currentQuestion.question; 
+
 
     // -- POPULATE THE ANSWERS INTO THE UI --
     // for each question container in the ui, store & pass in both the current container and it's index...
     // ...so it can be used to map the answers to the container innerText
     // ENHANCEMENT: eventually in React I may want to modify this to first check for the number of answers, then render that number of answer container elements into the ui - THEN perform this action.
-    answerChoiceContainer.forEach((currentContainer, currentIndex) => {
+    answerChoiceContainer.forEach(( currentContainer, currentIndex ) => {
 
       // while looping through each answerChoiceContainer ui element, replace the current container's innerText with an answer from the json's answer_choices array that matches the current container's index
-      currentContainer.innerText = currentQuestion.answer_choices[currentIndex]; 
+      currentContainer.innerText = currentQuestion.answer_choices[ currentIndex ]; 
+
+      // set the correct answer for the current question
+      correctAnswer = currentQuestion.correct_answer.toString();
     });
 
+    // remove the question we just used from the array so we don't duplicate questions during the quiz
+    // Cannot read properties of undefined (reading 'question') error will occur when we have run out of availableQuestions
+    availableQuestions.splice( questionIndex, 1 );
+
+    // after the question is loaded, allow user to answer question
+    isAcceptingAnswers = true;
   }
+
+
+  /* --- LISTENERS FOR UI USER ACTIONS --------- */
+
+  // add a click event listener to each answer element in the ui so we can identify which answer the user selects and check if it's correct
+  answerChoiceContainer.forEach( answer => {
+    answer.addEventListener( 'click', e => {
+
+      // ignore click if question is not loaded yet - adds a very slight delay 
+      if (!isAcceptingAnswers) return;
+
+      isAcceptingAnswers = false;
+      const answerSelectedByUser = e.target.innerText;
+      // const correct_answer == answerSelectedByUser.innerText
+      console.log(answerSelectedByUser);
+
+      // load new question
+      getNewQuestion();
+    } );
+  });
 
 startGame();
